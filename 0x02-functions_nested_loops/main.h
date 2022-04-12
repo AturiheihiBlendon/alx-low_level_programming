1cd18debeb5aa42baf6e59cad28b7785e42147a3
AturiheihiBlendon <2020blendon@gmail.com>
2022-04-12 11:58:38 -0700

    Updated main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,8 @@
  *
  * @c: The character to print
  *
- * Return: On succes 1
+ * Return: On succes 1.
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char);
 void print_alphabet(void);
