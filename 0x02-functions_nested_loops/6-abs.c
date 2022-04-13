189b2f5a460aad5d58282a466accd8294f8be962
AturiheihiBlendon <2020blendon@gmail.com>
2022-04-12 20:26:22 -0700

    Compute absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ *
+ * @n: to be checked
+ * Return: ALwyas 0.
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+	return (0);
+}
