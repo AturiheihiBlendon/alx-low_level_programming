f032a6322d9c6257bd8a4cab79c0551f9de1e8e0
Aturiheihi Blendon <73010408+AturiheihiBlendon@users.noreply.github.com>
2022-04-24 08:13:58 +0300

    concatenates two strings by strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
-
-	while (dest[dest_len] != '\0')
-		dest_len++;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-		dest[dest_len + i] = '\0';
-		return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
