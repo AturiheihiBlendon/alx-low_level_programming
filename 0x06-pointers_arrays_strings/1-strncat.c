d9c6b45288021d43db249a50ba7ef5a83c93a76d
AturiheihiBlendon <2020blendon@gmail.com>
2022-04-23 21:49:51 -0700

    concatenates two strings by strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,6 +9,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int dest_len, i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
