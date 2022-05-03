77e5618cb2d4c7835cced69ccffd92d42c61bb56
AturiheihiBlendon <2020blendon@gmail.com>
2022-05-03 19:18:42 +0000

    Print all arguments received

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,10 +6,13 @@
  * @argv: pointer to a string
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int j;
-for (j = 0; j < argc; j++)
-printf("%s\n", argv[j]);
-return (0);
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s\n", argv[j]);
+	}
+	return (0);
 }
