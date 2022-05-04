19e6998140055fecf2e6572d512596b7992a29ea
Aturiheihi Blendon <73010408+AturiheihiBlendon@users.noreply.github.com>
2022-05-04 18:00:41 +0300

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,14 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: The string to be concatenated upon.
- * @s2: The string to be concatenated to s1.
+ * str_concat - Concatenates two strings
+ * @s1: The string to be concatenated upon
+ * @s2: The string to be concatenated to s1
  *
- * Return: If concatenation fails - NULL.
- *         Otherwise - a pointer the newly-allocated space in memory
- *                     containing the concatenated strings.
+ * Return: If concatenation fails - NULL
  */
 char *str_concat(char *s1, char *s2)
 {
