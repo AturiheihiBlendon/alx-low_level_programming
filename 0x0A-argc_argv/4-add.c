78df992faf64a995e289f5b026edba265e7d719e
AturiheihiBlendon <2020blendon@gmail.com>
2022-05-04 03:15:14 +0000

    Adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,19 +10,20 @@
  */
 int main(int argc, char **argv)
 {
-int num, i, sum = 0;
-for (num = 1; num < argc; num++)
-{
-for (i = 0; argv[num][i]; i++)
-{
-if (argv[num][i] < '0' || argv[num][i] > '9')
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[num]);
-}
-printf("%d\n", sum);
-return (0);
+	int num, i, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (i = 0; argv[num][i]; i++)
+		{
+			if (argv[num][i] < '0' || argv[num][i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[num]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
