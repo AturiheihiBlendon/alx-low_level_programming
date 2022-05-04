8493ab4ec49ceaf7c4a6739b3873242d4f7ed5b4
Aturiheihi Blendon <73010408+AturiheihiBlendon@users.noreply.github.com>
2022-05-04 17:46:28 +0300

    creates array of char and initialize with specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = malloc(sizeof(ptr) * size);
+	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
