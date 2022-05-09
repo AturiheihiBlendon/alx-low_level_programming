ec61e24e3f5b6635a88c6b92eff24939746b4d94
AturiheihiBlendon <2020blendon@gmail.com>
2022-05-09 13:40:38 +0000

    Structure of my dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - my dog
+  *@name: name of my dog
+  *@owner: owner of my dog
+  *@age: age of my dog
+  */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
