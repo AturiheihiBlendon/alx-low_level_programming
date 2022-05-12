b284b7f005bce1a3ce210570bd4ed050d2032a67
Aturiheihi Blendon <73010408+AturiheihiBlendon@users.noreply.github.com>
2022-05-12 21:29:35 +0300

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,11 +15,10 @@ unsigned int index;
 va_start(nums, n);
 for (index = 0; index < n; index++)
 {
-	printf("%d", va_arg(nums, int));
-	if (index != (n - 1) && separator != NULL)
-	{	printf("%s", separator);
-	}
-	printf("\n");
-	va_end(nums);
+printf("%d", va_arg(nums, int));
+if (index != (n - 1) && separator != NULL)
+printf("%s", separator);
 }
+printf("\n");
+va_end(nums);
 }
