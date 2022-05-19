a91f8eb1a601fabdc371a4805b47011bfc0df6dd
AturiheihiBlendon <2020blendon@gmail.com>
2022-05-19 18:15:51 +0000

    Initial commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,12 @@
 #define LISTS_H
 #include <stdlib.h>
 
+/**
+  *struct list_s -structure of list_s
+  *@str: string
+  *@len: lenght of string
+  *@next: points to next node
+  */
 typedef struct list_s
 {
 	char *str;
@@ -15,4 +21,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif 
+#endif
