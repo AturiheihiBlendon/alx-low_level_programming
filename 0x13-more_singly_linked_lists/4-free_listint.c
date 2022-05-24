8c66c097e84392e5bc9a69e5c068998e281f97ce
AturiheihiBlendon <2020blendon@gmail.com>
2022-05-24 15:50:58 +0000

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,4 +1,3 @@
-
 #include "lists.h"
 
 /**
@@ -7,11 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-listint_t *tmp;
-while (head)
-{
-tmp = head->next;
-free(head);
-head = tmp;
-}
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
